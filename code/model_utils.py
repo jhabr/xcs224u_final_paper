467a88ebb562451335fb97bb5668ce4bbec5b599
antongochev <gochev.anton@gmail.com>
2021-03-12 20:05:18 +0100

    improve the methods, add method to subset examples and simplify the notebook

--- code/model_utils.py
+++ code/model_utils.py
@@ -6,8 +6,8 @@ from transformers import (
     ElectraTokenizer, ElectraModel,
     RobertaTokenizer, RobertaModel 
 )
-
-from utils import START_SYMBOL, END_SYMBOL
+import utils
+from utils import START_SYMBOL, END_SYMBOL, UNK_SYMBOL
 
 __authors__ = "Anton Gochev, Jaro Habr, Yan Jiang, Samuel Kahn"
 __version__ = "XCS224u, Stanford, Winter 2021"
@@ -34,11 +34,14 @@ def extract_input_embeddings(colour_texts, model, tokenizer, strip_punct=True, s
 
     Returns
     -------
-        A dictionannary containing the embeddings. A list containing the vocab.
+        A list of vectors that is the embeddings of the model. A list of token types that reperesnet
+        the vocab.
 
     """
 
     embeddings = model.get_input_embeddings()
+    model_embeddings = []
+    model_vocab = []
     result = dict()
         
     for ct in colour_texts:
@@ -53,12 +56,22 @@ def extract_input_embeddings(colour_texts, model, tokenizer, strip_punct=True, s
             if strip_symbols:
                 input_token = strip_special_symbols(input_token)
 
-            if input_token not in result:
-                result[input_token] = vectors[0][i]
-                    
-    return result, list(result.keys())
+            if input_token not in model_vocab:
+                model_vocab.append(input_token)
+                model_embeddings.append(vectors[0][i].detach().numpy())
 
-def extract_contextual_embeddings(colour_texts, model, tokenizer, strip_punct=True, strip_symbols=True):
+    # add the special symbols and associated random vectors required for the model
+    # to understand the end and start of utterences and an uknown vector
+    model_vocab.append(UNK_SYMBOL)
+    model_vocab.append(START_SYMBOL)
+    model_vocab.append(END_SYMBOL)
+
+    for i in range(3):
+        model_embeddings.append(utils.randvec(len(model_embeddings[0])))
+
+    return model_embeddings, model_vocab
+
+def extract_positional_embeddings(colour_texts, model, tokenizer, strip_punct=True, strip_symbols=True):
     """
     Parameters 
     ----------
@@ -80,13 +93,13 @@ def extract_contextual_embeddings(colour_texts, model, tokenizer, strip_punct=Tr
 
     Returns
     -------
-        A list containing the embeddings [token_<position>, vector] and a list containing the vocab [type of token].
+        A list of vectors that is the embeddings of the model. A list of token types that reperesnet
+        the vocab.
 
     """
 
-    embeddings = model.get_input_embeddings()
-    result_embeddings = []
-    result_vocab = dict()
+    model_embeddings = []
+    model_vocab = []
         
     for ct in colour_texts:
         if strip_punct == True:
@@ -94,19 +107,29 @@ def extract_contextual_embeddings(colour_texts, model, tokenizer, strip_punct=Tr
         input_ids = torch.tensor(tokenizer.encode(ct, add_special_tokens=False)).unsqueeze(0)
         input_tokens = tokenizer.convert_ids_to_tokens(input_ids[0])        
         outputs = get_model_outputs(model, input_ids)        
+        # hidden_states is the output of 
         vectors = outputs.hidden_states[0]
-                
+
         for i in range(len(input_tokens)):
             if strip_symbols:
                 input_token = strip_special_symbols(input_tokens[i])
 
             input_token = input_token + '_' + str(i)
 
-            if input_token not in result_vocab:
-                result_vocab[input_token] = input_token
-                result_embeddings.append([input_token, vectors[0][i]])
+            if input_token not in model_vocab:
+                model_vocab.append(input_token)
+                model_embeddings.append(vectors[0][i].detach().numpy())
                     
-    return result_embeddings, list(result_vocab.keys())
+    # add the special symbols and associated random vectors required for the model
+    # to understand the end and start of utterences and an uknown vector
+    model_vocab.append(UNK_SYMBOL)
+    model_vocab.append(START_SYMBOL)
+    model_vocab.append(END_SYMBOL)
+
+    for i in range(3):
+        model_embeddings.append(utils.randvec(len(model_embeddings[0])))
+
+    return model_embeddings, model_vocab
 
 def get_model_outputs(model, input_ids):
     """
@@ -126,9 +149,32 @@ def get_model_outputs(model, input_ids):
         return model(input_ids=input_ids, decoder_input_ids=input_ids, output_hidden_states=True)
     else:
         return model(input_ids=input_ids, output_hidden_states=True)
-    
 
-def tokenize_colour_description(s, tokenizer):
+
+def extract_colour_examples(examples, from_word_count=5):    
+    """
+    Extracts all colour examples with more than 'from_word_count' words.
+
+    Parameters 
+    ----------
+    
+    examples: colors.ColorsCorpusExample
+        A list of colour examples to subset from.
+
+    Returns
+    -------
+        A list of coulours examples.
+
+    """
+    result = []
+    for row in examples:
+        if row.contents.count(" ") > from_word_count:
+            result.append(row)
+
+    return result
+
+
+def tokenize_colour_description(s, tokenizer, include_position=False):
     """
     Parameters 
     ----------
@@ -138,6 +184,13 @@ def tokenize_colour_description(s, tokenizer):
 
     model: huggingface transformer model
         Huggingface trasnformer model to be used for extracting the tokens to be sequenced.
+
+    include_position: Boolean
+        For contextual embeddings this parameter should be set to True and the position of the 
+        token will be added to the token itself [token_i]. This is important as this is the way
+        the contextual embeddings and vocab are constructed. 
+        For pre-trained embeddings it should be left to False and the position of the token will 
+        not be included.
 
     Returns
     -------
@@ -150,7 +203,11 @@ def tokenize_colour_description(s, tokenizer):
     
     result = []
     for i in range(len(input_tokens)):
-        result.append(strip_special_symbols(input_tokens[i]))
+        input_token = input_tokens[i]
+        if include_position:
+            input_token = input_token + '_' + str(i)
+
+        result.append(strip_special_symbols(input_token))
 
     return [START_SYMBOL] + result + [END_SYMBOL]
 
