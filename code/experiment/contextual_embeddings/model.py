be98f345bf1219744adcfb81068c6f5478852a31
Jaro Habr <jaro@freshbits.ch>
2021-03-15 23:39:01 +0100

    Trying some other stuff

--- code/experiment/contextual_embeddings/model.py
+++ code/experiment/contextual_embeddings/model.py
@@ -1,8 +1,9 @@
 import torch
 from torch import nn
 from transformers import PreTrainedTokenizer, PreTrainedModel
+import numpy as np
 
-from utils.torch_color_describer import Decoder, ContextualColorDescriber, Encoder, EncoderDecoder
+from utils.torch_color_describer import Decoder, ContextualColorDescriber, Encoder, EncoderDecoder, ColorDataset
 from utils.utils import START_SYMBOL, END_SYMBOL, UNK_SYMBOL
 
 
@@ -19,9 +20,11 @@ class ContextualDecoder(Decoder):
     This class represents the contextual system decoder.
     """
 
-    def __init__(self, color_dim, *args, **kwargs):
+    def __init__(self, color_dim, tokenizer: PreTrainedTokenizer, model: PreTrainedModel, *args, **kwargs):
         self.color_dim = color_dim
         super().__init__(*args, **kwargs)
+        self.tokenizer = tokenizer
+        self.model = model
 
         self.rnn = nn.GRU(
             input_size=self.embed_dim + self.color_dim,
@@ -32,7 +35,7 @@ class ContextualDecoder(Decoder):
     def get_embeddings(self, word_seqs, target_colors=None):
         _, repeats = word_seqs.size()
 
-        embeddings = self.embedding(word_seqs)
+        embeddings = self.__extract_embeddings(word_seqs)
 
         target_colors_reshaped = torch.repeat_interleave(
             target_colors, repeats, dim=0
@@ -48,6 +51,20 @@ class ContextualDecoder(Decoder):
         assert result.shape == expected.shape, f"Expected shape {expected.shape}; got shape {result.shape}"
 
         return result
+
+    def __extract_embeddings(self, input_ids):
+        embeddings = []
+
+        for ids in input_ids:
+            input_ids = torch.LongTensor(ids).unsqueeze(0)
+            attention_mask = torch.LongTensor([1] * len(ids)).unsqueeze(0)
+
+            # sequence = self.tokenizer.decode(ids)
+            # inputs = self.tokenizer(sequence, add_special_tokens=False, output_hidden_states=True, return_tensors="pt")
+            output = self.model(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True)
+            embeddings.append(output.hidden_states[12].squeeze(0))
+
+        return torch.stack(embeddings)
 
 
 class ContextualEncoderDecoder(EncoderDecoder):
@@ -83,23 +100,67 @@ class ContextualDescriber(ContextualColorDescriber):
     def __init__(self, tokenizer: PreTrainedTokenizer, model: PreTrainedModel, early_stopping, freeze_embedding=False):
         self.tokenizer = tokenizer
         self.model = model
-        self.__update_vocab__()
+        # self.__update_vocab__()
+        self.vocab = self.tokenizer.vocab
 
         super().__init__(
             vocab=tokenizer.vocab,
             embed_dim=768,
             early_stopping=early_stopping,
             freeze_embedding=freeze_embedding,
-            start_index=self.tokenizer.vocab[START_SYMBOL],
-            end_index=self.tokenizer.vocab[END_SYMBOL],
-            unk_index=self.tokenizer.vocab[UNK_SYMBOL]
+            # start_index=self.tokenizer.convert_tokens_to_ids(START_SYMBOL),
+            # end_index=self.tokenizer.convert_tokens_to_ids(END_SYMBOL),
+            # unk_index=self.tokenizer.convert_tokens_to_ids(UNK_SYMBOL)
+            start_index=self.tokenizer.cls_token_id,
+            end_index=self.tokenizer.sep_token_id,
+            unk_index=self.tokenizer.unk_token_id
         )
 
-    def __update_vocab__(self):
-        length = len(self.tokenizer.vocab.keys())
-        self.tokenizer.vocab[START_SYMBOL] = length + 1
-        self.tokenizer.vocab[END_SYMBOL] = length + 1
-        self.tokenizer.vocab[UNK_SYMBOL] = length + 1
+    # def __update_vocab__(self):
+    #     self.tokenizer.vocab[START_SYMBOL] = self.tokenizer.vocab_size + 1
+    #     self.tokenizer.vocab[END_SYMBOL] = self.tokenizer.vocab_size + 1
+    #     self.tokenizer.vocab[UNK_SYMBOL] = self.tokenizer.vocab_size + 1
+
+    def build_dataset(self, color_seqs, word_seqs):
+        self.color_dim = len(color_seqs[0][0])
+        input_ids = []
+
+        for sequence in word_seqs:
+            seq_input_ids = self.tokenizer.encode(sequence, add_special_tokens=True)
+            # seq_input_ids = [self.vocab[START_SYMBOL]] + seq_input_ids + [self.vocab[END_SYMBOL]]
+            input_ids.append(seq_input_ids)
+
+        # word_seqs = [[self.word2index.get(w, self.unk_index) for w in seq] for seq in word_seqs]
+
+        ex_lengths = [len(input_id) for input_id in input_ids]
+
+        return ColorDataset(color_seqs, input_ids, ex_lengths)
+
+    def _convert_predictions(self, pred):
+        # rep = []
+        # for i in pred:
+        #     i = i.item()
+        #     rep.append(self.tokenizer.convert_ids_to_tokens(i))
+        #     if i == self.end_index:
+        #         return rep
+        # return rep
+        return self.tokenizer.decode(pred)
+
+    def perplexities(self, color_seqs, word_seqs, device=None):
+        probs = self.predict_proba(color_seqs, word_seqs, device=device)
+        scores = []
+        for prediction, sequence in zip(probs, word_seqs):
+            s = np.array([t[w] for t, w in zip(prediction, self.tokenizer.encode(sequence, add_special_tokens=True))])
+            scores.append(s)
+        perp = [np.prod(s) ** (-1 / len(s)) for s in scores]
+        return perp
+
+        # for pred, seq in zip(probs, word_seqs):
+        #     # Get the probabilities corresponding to the path `seq`:
+        #     s = np.array([t[self.word2index.get(w, self.unk_index)] for t, w in zip(pred, seq)])
+        #     scores.append(s)
+        # perp = [np.prod(s) ** (-1 / len(s)) for s in scores]
+        # return perp
 
     def build_graph(self):
         encoder = ContextualEncoder(
@@ -112,7 +173,9 @@ class ContextualDescriber(ContextualColorDescriber):
             embed_dim=self.embed_dim,
             embedding=self.embedding,
             hidden_dim=self.hidden_dim,
-            color_dim=self.color_dim
+            color_dim=self.color_dim,
+            tokenizer=self.tokenizer,
+            model=self.model
         )
 
         return ContextualEncoderDecoder(encoder, decoder)
--- code/experiment/contextual_embeddings/test_contextual.py
+++ code/experiment/contextual_embeddings/test_contextual.py
@@ -3,12 +3,14 @@ from transformers import BertTokenizer, BertModel
 
 from experiment.contextual_embeddings.model import ContextualDescriber
 from utils.torch_color_describer import create_example_dataset
+from utils.utils import START_SYMBOL, END_SYMBOL, UNK_SYMBOL
 
 
 def create_dev_data():
     dev_color_seqs, dev_word_seqs, _ = create_example_dataset(
         group_size=50,
-        vec_dim=2
+        vec_dim=2,
+        add_special_tokens=False
     )
 
     dev_sentences = ["".join(dev_word_seq) for dev_word_seq in dev_word_seqs]
@@ -17,10 +19,21 @@ def create_dev_data():
 
 
 if __name__ == '__main__':
-    dev_colors_train, dev_colors_test, dev_sentences_train, dev_sentences_test = \
-        create_dev_data()
+    dev_colors_train, dev_colors_test, dev_sentences_train, dev_sentences_test = create_dev_data()
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # tokenizer.added_tokens_encoder = {
+    #     '<s>': tokenizer.vocab_size + 1,
+    #     '</s>': tokenizer.vocab_size + 1,
+    #     '$UNK': tokenizer.vocab_size + 1
+    # }
+    # tokenizer.add_tokens([START_SYMBOL, END_SYMBOL, UNK_SYMBOL], special_tokens=True)
+    # tokenizer.cls_token = START_SYMBOL
+    # #tokenizer.cls_token_id = tokenizer.vocab["[CLS]"]
+    # tokenizer.sep_token = END_SYMBOL
+    # #tokenizer.sep_token_id = tokenizer.vocab["[SEP]"]
+    # tokenizer.unk_token = UNK_SYMBOL
+    # #tokenizer.unk_token_id = tokenizer.vocab["[UNK]"]
     model = BertModel.from_pretrained('bert-base-uncased')
 
     dev_contextual_model = ContextualDescriber(
@@ -30,3 +43,6 @@ if __name__ == '__main__':
     )
 
     dev_contextual_model.fit(dev_colors_train, dev_sentences_train)
+
+    print()
+    print(dev_contextual_model.evaluate(dev_colors_test, dev_sentences_test))
