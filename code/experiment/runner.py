c3baa54b89c1b6cc5c60925b29ad3e0d5bd3c9eb
Jaro Habr <jaro@freshbits.ch>
2021-03-19 00:26:05 +0100

    Add decoder dropout regularization

--- code/experiment/runner.py
+++ code/experiment/runner.py
@@ -3,4 +3,6 @@ from experiment.experiment_library import ExperimentLibrary
 if __name__ == '__main__':
     experiment_library = ExperimentLibrary()
 
-    experiment_library.run_baseline(debug=True, run_bake_off=True)
+    # experiment_library.run_baseline(debug=False, run_bake_off=True)
+
+    experiment_library.run_baseline(debug=True, decoder_drop_out=0.15, run_bake_off=True)
