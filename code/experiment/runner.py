809c2f74dffb08a34b781295748faa0fd96b9dc2
Jaro Habr <jaro@freshbits.ch>
2021-03-24 21:36:45 +0100

    Add experiment

--- code/experiment/runner.py
+++ code/experiment/runner.py
@@ -8,5 +8,6 @@ if __name__ == '__main__':
     # ExperimentLibrary.run_vision_with_fourier_baseline(debug=True)
 
     # transformers
-    ExperimentLibrary.run_vision_bert_last_layers_sum(debug=True)
+    ExperimentLibrary.run_fourier_bert_last_layers_sum(debug=True)
+    #ExperimentLibrary.run_vision_bert_last_layers_sum(debug=True)
 
