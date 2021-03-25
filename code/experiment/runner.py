0ac3f7eb601d5de50d5eb3c7118262fe679db611
Jaro Habr <jaro@freshbits.ch>
2021-03-25 22:28:49 +0100

    Fix typo

--- code/experiment/runner.py
+++ code/experiment/runner.py
@@ -4,7 +4,7 @@ from experiment.experiment_library import ExperimentLibrary
 
 
 def set_python_path():
-    if os.getenv('PYTHONPATH') in None:
+    if os.getenv('PYTHONPATH') is None:
         head, _ = os.path.split(os.path.abspath(os.getcwd()))
         os.environ['PYTHONPATH'] = head
 
