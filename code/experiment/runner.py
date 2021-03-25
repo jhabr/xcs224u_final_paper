6e01872cb0981093bbbcd4b8f5db8e0717b4603c
Jaro Habr <jaro@freshbits.ch>
2021-03-25 09:55:48 +0100

    Run experiment without debug mode

--- code/experiment/runner.py
+++ code/experiment/runner.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     # ExperimentLibrary.run_vision_with_fourier_baseline(debug=True)
 
     # transformers
-    ExperimentLibrary.run_fourier_bert_last_layers_sum(debug=True)
+    ExperimentLibrary.run_fourier_bert_last_layers_sum(debug=False)
     #ExperimentLibrary.run_vision_bert_last_layers_sum(debug=True)
 
