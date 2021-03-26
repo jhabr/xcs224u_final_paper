8e2530a6de68d6bbdca46e9766a5b35e79329404
Jaro Habr <jaro@freshbits.ch>
2021-03-26 10:49:30 +0100

    Add print statement

--- code/experiment/__init__.py
+++ code/experiment/__init__.py
@@ -2,6 +2,8 @@ import os
 
 
 def set_python_path():
+    print("Setting <PYTHONPATH>...")
+
     if os.getenv('PYTHONPATH') is None:
         head, _ = os.path.split(os.path.abspath(os.getcwd()))
         os.environ['PYTHONPATH'] = head
